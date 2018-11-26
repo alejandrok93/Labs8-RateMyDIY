e7edd0b6ee9517b82b8963e8bdb330f0a2b3c695
alejandrok93 <alejandrok93@gmail.com>
2018-11-26 15:53:29 -0600

    Added fetchSearchResults action to landing page

--- reactapp/src/components/_LandingPage/LandingPage.js
+++ reactapp/src/components/_LandingPage/LandingPage.js
@@ -1,90 +1,89 @@
 // Import Dependencies
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // import { NavLink, Link, Route } from "react-router-dom";
-import styled from "styled-components";
-import { connect } from "react-redux";
-import { getReviewers } from "../../actions";
-
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { getReviewers, fetchSearchResults } from '../../actions';
 
 //Import components
 import {
-  DropDown,
-  FeaturedProjects,
-  PopularMakers,
-  PopularReviewers,
-  SearchBar,
-  Twillio
-} from "../../components";
+	DropDown,
+	FeaturedProjects,
+	PopularMakers,
+	PopularReviewers,
+	SearchBar,
+	Twillio
+} from '../../components';
 
 // styled-components
 const LandingPageContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-  width: 80%;
+	display: flex;
+	flex-direction: column;
+	margin: auto;
+	width: 80%;
 `;
 const LandingPageWrapper = styled.div`
-  width: 100%;
+	width: 100%;
 `;
 const DropdownMenu = styled.div`
-  width: 100%;
+	width: 100%;
 `;
 
 class LandingPage extends Component {
-  constructor() {
-    super();
-    this.state = { input: "" };
-  }
+	constructor() {
+		super();
+		this.state = { input: '' };
+	}
 
-  componentDidMount() {
-    console.log("component did mount!");
-    this.props.getReviewers();
-  }
+	componentDidMount() {
+		console.log('component did mount!');
+		this.props.getReviewers();
+	}
 
-  handleChange = e => {
-    console.log(e.target.value);
-    this.setState({ ...this.state, input: e.target.value });
-  };
+	handleChange = e => {
+		console.log(e.target.value);
+		this.setState({ ...this.state, input: e.target.value });
+	};
 
-  handleSearch = e => {
-    //call featch search results action
-    this.props.fetchSearchResults(this.state.input);
+	handleSearch = e => {
+		//call featch search results action
+		this.props.fetchSearchResults(this.state.input);
 
-    //push to search page
-    this.props.history.push("/search");
-  };
+		//push to search page
+		this.props.history.push('/search');
+	};
 
-  render() {
-    // console.log(SearchBar);
-    return (
-      <LandingPageWrapper>
-        <DropdownMenu>
-          <DropDown />
-        </DropdownMenu>
-        <LandingPageContentWrapper>
-          <SearchBar
-            handleChange={this.handleChange}
-            handleSearch={this.handleSearch}
-          />
-          <Twillio />
-          <FeaturedProjects featuredProjects={this.props.featuredProjects} />
-          <PopularMakers popularMakers={this.props.popularMakers} />
-          <PopularReviewers reviewers={this.props.popularReviewers} />
-        </LandingPageContentWrapper>
-      </LandingPageWrapper>
-    );
-  }
+	render() {
+		// console.log(SearchBar);
+		return (
+			<LandingPageWrapper>
+				<DropdownMenu>
+					<DropDown />
+				</DropdownMenu>
+				<LandingPageContentWrapper>
+					<SearchBar
+						handleChange={this.handleChange}
+						handleSearch={this.handleSearch}
+					/>
+					<Twillio />
+					<FeaturedProjects featuredProjects={this.props.featuredProjects} />
+					<PopularMakers popularMakers={this.props.popularMakers} />
+					<PopularReviewers reviewers={this.props.popularReviewers} />
+				</LandingPageContentWrapper>
+			</LandingPageWrapper>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    featuredProjects: state.landingPageReducer.featuredProjects,
-    popularMakers: state.landingPageReducer.popularMakers,
-    popularReviewers: state.landingPageReducer.popularReviewers
-  };
+	return {
+		featuredProjects: state.landingPageReducer.featuredProjects,
+		popularMakers: state.landingPageReducer.popularMakers,
+		popularReviewers: state.landingPageReducer.popularReviewers
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  { getReviewers }
+	mapStateToProps,
+	{ getReviewers, fetchSearchResults }
 )(LandingPage);

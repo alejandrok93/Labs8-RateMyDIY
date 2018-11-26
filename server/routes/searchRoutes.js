ed44a37a5edf680ad79c6a15f85761cb0f1cca1a
alejandrok93 <alejandrok93@gmail.com>
2018-11-26 13:30:45 -0600

    Search model created that retrieves project data from database, uses Fuse library to search

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../models/projectModel");
+const db = require("../models/searchModel");
 
 //Search API endpoint
 router.get("/", (req, res) => {
   const query = req.query.query;
   console.log("query:" + query);
-  res.status(200).json({ projects: { id: 1 } });
+  db.getSearchResults(query)
+    .then(results => {
+      console.log(results);
+      res.status(200).json(results);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;

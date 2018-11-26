37f086e93047f631064a024cad049c4d5cd84478
michaelagard <mdagard@gmail.com>
2018-11-26 12:32:07 -0500

    del project_id code, fixed error message

--- server/routes/landingPageRoutes.js
+++ server/routes/landingPageRoutes.js
@@ -5,14 +5,13 @@ const db = require('../models/landingPageModel');
 
 // get featured projects
 router.get('/projects', function (req, res) {
-  const { project_id } = req.params;
 
   db.getLandingPageProjects()
     .then(project => {
       if (project) {
         res.status(200).json(project);
       } else {
-        res.status(404).json({ error: 'Project not found.' });
+        res.status(404).json({ error: 'Projects not found.' });
       }
     })
     .catch(err => {

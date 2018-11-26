aa27f2e83f2f45a91cf0fa4832b9d6a0b9b1c005
michaelagard <mdagard@gmail.com>
2018-11-26 15:38:26 -0500

    add makers endpoint for lp

--- server/routes/landingPageRoutes.js
+++ server/routes/landingPageRoutes.js
@@ -5,13 +5,26 @@ const db = require('../models/landingPageModel');
 
 // get featured projects
 router.get('/projects', function (req, res) {
-
-  db.getLandingPageProjects()
+  db.getPopularProjects()
     .then(project => {
       if (project) {
         res.status(200).json(project);
       } else {
         res.status(404).json({ error: 'Projects not found.' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get('/makers', function (req, res) {
+  db.getPopularMakers()
+    .then(maker => {
+      if (maker) {
+        res.status(200).json(maker);
+      } else {
+        res.status(404).json({ error: 'Makers not found.' });
       }
     })
     .catch(err => {

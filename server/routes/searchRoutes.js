aef3cfb362a32ae140d819c9dc0ac4ccbb598b72
alejandrok93 <alejandrok93@gmail.com>
2018-11-26 13:33:21 -0600

    Added check if query exists

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -5,14 +5,21 @@ const db = require("../models/searchModel");
 
 //Search API endpoint
 router.get("/", (req, res) => {
+  //get search query from URL
   const query = req.query.query;
-  console.log("query:" + query);
-  db.getSearchResults(query)
-    .then(results => {
-      console.log(results);
-      res.status(200).json(results);
-    })
-    .catch(err => res.status(500).json(err));
+
+  //check if query exists
+  if (!query) {
+    res.status(400).json({ message: "Bad request" });
+  }
+  if (query) {
+    db.getSearchResults(query)
+      .then(results => {
+        console.log(results);
+        res.status(200).json(results);
+      })
+      .catch(err => res.status(500).json(err));
+  }
 });
 
 module.exports = router;

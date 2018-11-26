cffa248c17423cc7496351b224c73839f1dfba18
alejandrok93 <alejandrok93@gmail.com>
2018-11-26 12:41:03 -0600

    Added API endpoint to retrieve search query from URL

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 // DEPENDENCIES
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-const db = require('./config/dbConfig');
-const twilio = require('twilio');
-const accountSid = 'AC5015bf59d92c66d960602ce3a51d6e1d';
-const authToken = 'df07b21ee914b8532f3dbc78fd8e32e8';
+const db = require("./config/dbConfig");
+const twilio = require("twilio");
+const accountSid = "AC5015bf59d92c66d960602ce3a51d6e1d";
+const authToken = "df07b21ee914b8532f3dbc78fd8e32e8";
 const client = new twilio(accountSid, authToken);
 
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 
 // MIDDLEWARE
-const configureMiddleware = require('./config/middleware');
+const configureMiddleware = require("./config/middleware");
 
 configureMiddleware(server);
 
@@ -22,88 +22,95 @@ configureMiddleware(server);
 // const exampleRoutes = require('./routes/exampleRoutes');
 
 // SANITY CHECK
-server.get('/', (req, res) => {
-	res.send(
-		`Believe it or not, this is the first endpoint added to the great RateMyDIY project.`
-	);
+server.get("/", (req, res) => {
+  res.send(
+    `Believe it or not, this is the first endpoint added to the great RateMyDIY project.`
+  );
 });
 
 // sendgrid test implementation
-server.post('/sendgrid/test', (req, res) => {
-	const recipient = req.body.to;
-	const msg = {
-		to: recipient,
-		from: 'ratemydyics@ratemydyi.com',
-		subject: 'Welcome to RateMyDIY',
-		text: `Thank you for subscribing to Rate My DIY mail service.`,
-		html:
-			'<strong>Thank you for subscribing to Rate My DIY mail service.</strong>'
-	};
-	if (!req.body.to) {
-		return res.status(422).json({ error: 'Email cannot be empty.' });
-	} else {
-		// pulls api key from .env file
-		sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-		// sends email based on message object set above.
-		sgMail
-			.send(msg)
-			.then(() => {
-				res
-					.status(200)
-					.json({ message: `Email successfully sent to ${recipient}` })
-					.end();
-			})
-			.catch(err => {
-				console.error(err.toString());
-				res.status(500).end();
-			});
-	}
+server.post("/sendgrid/test", (req, res) => {
+  const recipient = req.body.to;
+  const msg = {
+    to: recipient,
+    from: "ratemydyics@ratemydyi.com",
+    subject: "Welcome to RateMyDIY",
+    text: `Thank you for subscribing to Rate My DIY mail service.`,
+    html:
+      "<strong>Thank you for subscribing to Rate My DIY mail service.</strong>"
+  };
+  if (!req.body.to) {
+    return res.status(422).json({ error: "Email cannot be empty." });
+  } else {
+    // pulls api key from .env file
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // sends email based on message object set above.
+    sgMail
+      .send(msg)
+      .then(() => {
+        res
+          .status(200)
+          .json({ message: `Email successfully sent to ${recipient}` })
+          .end();
+      })
+      .catch(err => {
+        console.error(err.toString());
+        res.status(500).end();
+      });
+  }
 });
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const postRoutes = require('./routes/postRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const postRoutes = require("./routes/postRoutes");
 
-server.use('/', authRoutes);
-server.use('/api/users', userRoutes);
-server.use('/api/projects', projectRoutes);
-server.use('/api/posts', postRoutes);
+server.use("/", authRoutes);
+server.use("/api/users", userRoutes);
+server.use("/api/projects", projectRoutes);
+server.use("/api/posts", postRoutes);
 
 //Twilio
-server.get('/send-text', (req, res) => {
-	//Welcome Message
-	res.send('Hello to the Twilio Server');
+server.get("/send-text", (req, res) => {
+  //Welcome Message
+  res.send("Hello to the Twilio Server");
 
-	//_GET Variables
-	const { recipient, textmessage } = req.query;
+  //_GET Variables
+  const { recipient, textmessage } = req.query;
 
-	//Send Text
-	client.messages
-		.create({
-			body: textmessage,
-			to: recipient, // Text this number
-			from: '+15625219688 ' // From a valid Twilio number
-		})
-		.then(message => console.log(message.sid));
+  //Send Text
+  client.messages
+    .create({
+      body: textmessage,
+      to: recipient, // Text this number
+      from: "+15625219688 " // From a valid Twilio number
+    })
+    .then(message => console.log(message.sid));
 });
 
 //Twilio
-server.get('/send-text', (req, res) => {
-	//Welcome Message
-	res.send('Hello to the Twilio Server');
+server.get("/send-text", (req, res) => {
+  //Welcome Message
+  res.send("Hello to the Twilio Server");
 
-	//_GET Variables
-	const { recipient, textmessage } = req.query;
+  //_GET Variables
+  const { recipient, textmessage } = req.query;
 
-	//Send Text
-	client.messages
-		.create({
-			body: textmessage,
-			to: recipient, // Text this number
-			from: '+15625219688 ' // From a valid Twilio number
-		})
-		.then(message => console.log(message.sid));
+  //Send Text
+  client.messages
+    .create({
+      body: textmessage,
+      to: recipient, // Text this number
+      from: "+15625219688 " // From a valid Twilio number
+    })
+    .then(message => console.log(message.sid));
+});
+
+//Search API endpoint
+server.get("/api/search", (req, res) => {
+  const query = req.query.query;
+  console.log("query:" + query);
+  res.status(200).json({ projects: { id: 1 } });
 });
 
 // server.use(function(req, res, next) {

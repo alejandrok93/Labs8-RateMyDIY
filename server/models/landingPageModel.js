e68e763be48b0ec91a6737a3159aa55547ae9a5c
michaelagard <mdagard@gmail.com>
2018-11-26 13:55:37 -0500

    add knex selector for top four rated projects

--- server/models/landingPageModel.js
+++ server/models/landingPageModel.js
@@ -6,6 +6,13 @@ module.exports = {
 
 function getLandingPageProjects() {
   return db('projects')
+    .join("users", "projects.user_id", "users.user_id")
+    .select("projects.img_url",
+      "projects.user_id",
+      "projects.project_name",
+      "users.username",
+      "projects.project_id",
+      "projects.project_rating")
     .orderBy('project_rating', 'desc')
     .limit(4)
 }

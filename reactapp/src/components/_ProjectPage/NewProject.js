4e8b7296857fdc9679a1c433bc939191c4fd593c
David Benavidez <benavidez_david13@yahoo.com>
2018-11-27 20:39:39 -0800

    hot-fix

--- reactapp/src/components/_ProjectPage/NewProject.js
+++ reactapp/src/components/_ProjectPage/NewProject.js
@@ -83,7 +83,7 @@ class NewProject extends Component {
         this.state.selectedFile.name
       );
       axios
-        .post(process.env.REACT_APP_BACKEND || 'http://localhost:5000/api/projects/image-upload', data, {
+        .post((process.env.REACT_APP_BACKEND || 'http://localhost:5000') + `/api/projects/image-upload`, data, {
           headers: {
             accept: 'application/json',
             'Accept-Language': 'en-US,en;q=0.8',

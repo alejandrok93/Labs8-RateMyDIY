2aea956488a57bfa799c208821d924d5eb696163
Michael Agard <oraclekai@gmail.com>
2018-11-29 17:34:44 -0500

    delete lines req.user.profile lines (#97)

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,26 +9,24 @@ const usersDB = require('../models/usersModel');
 
 const authenticate = require('../config/authMiddleware');
 
-router.get('/user', function(req, res, next) {
+router.get('/user', function (req, res, next) {
 	res.status(200).json(req.cookies);
 });
 
 router.post('/change', function (req, res, next) {
-  	const sub = req.user.profile._json.sub;
 	const auth_id = sub.split('|')[1];
 	const { username } = req.body;
-	const img_url = req.user.profile._json.picture;
 	const user = {
 		auth_id,
 		username,
 		img_url
-  	};
-  
-  	usersDB
-		  .checkUsernames(username)
-		  .then(usernameList => {
-			  console.log('usernameList', usernameList);
-			  if (usernameList.length === 0) {
+	};
+
+	usersDB
+		.checkUsernames(username)
+		.then(usernameList => {
+			console.log('usernameList', usernameList);
+			if (usernameList.length === 0) {
 				usersDB
 					.addUser(user)
 					.then(dbRes => {
@@ -39,14 +37,14 @@ router.post('/change', function (req, res, next) {
 						console.log('DB ERROR');
 						res.status(500).json({ error: 'Could not add to database' });
 					});
-			  } else {
+			} else {
 				res.status(400).json({ error: 'Please enter a different username' });
-			  }
-		  })
-		  .catch(usernameError => {
-			  console.log('usernameError', usernameError);
-			  res.status(500).json({ error: 'Could not test username' });
-			});
+			}
+		})
+		.catch(usernameError => {
+			console.log('usernameError', usernameError);
+			res.status(500).json({ error: 'Could not test username' });
+		});
 });
 
 router.post('/myprojects', authenticate, function (req, res, next) {

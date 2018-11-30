cd7f016f1f16341790d3ea908bc077f668717af0
alejandrok93 <alejandrok93@gmail.com>
2018-11-30 12:44:06 -0800

    Moved Twillio text integration to settings page

--- reactapp/src/components/_LandingPage/LandingPage.js
+++ reactapp/src/components/_LandingPage/LandingPage.js
@@ -106,7 +106,6 @@ class LandingPage extends Component {
 	};
 
 	render() {
-		console.log(this.state.modal);
 		return (
 			<LandingPageWrapper>
 				{window.innerWidth <= 500 ? <MenuDrawer /> : <Nav />}
@@ -124,7 +123,6 @@ class LandingPage extends Component {
 						//  'hey please log in'
 						''
 					)}
-					<Twillio />
 					<FeaturedProjects />
 					<PopularMakers fetchSearchResults={this.searchClick} />
 					<PopularReviewers

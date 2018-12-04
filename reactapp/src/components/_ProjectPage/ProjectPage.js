321bc01c18c03598ce02814237803737a793225e
Max <maxkajiwara@gmail.com>
2018-12-03 20:04:31 -0800

    Remove redirect when leaving ProjectPage

--- reactapp/src/components/_ProjectPage/ProjectPage.js
+++ reactapp/src/components/_ProjectPage/ProjectPage.js
@@ -26,7 +26,8 @@ import {
 	deleteProject,
 	willAddPost,
 	willUpdatePost,
-	willDeletePost
+	willDeletePost,
+	setRedirect
 } from '../../actions';
 
 // Styles
@@ -107,6 +108,10 @@ class ProjectPage extends Component {
 	componentDidMount() {
 		// Trying out this awkward action chain
 		this.props.loggedIn_Project_ReviewId_Chain(this.props.match.params.id);
+	}
+
+	componentWillUnmount() {
+		this.props.setRedirect(null);
 	}
 
 	render() {
@@ -310,6 +315,7 @@ export default connect(
 		deleteProject,
 		willAddPost,
 		willUpdatePost,
-		willDeletePost
+		willDeletePost,
+		setRedirect
 	}
 )(ProjectPage);

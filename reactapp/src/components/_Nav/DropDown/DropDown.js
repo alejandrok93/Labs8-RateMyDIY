f67bf02ec1cb51c06b9fc3a3a0f27758193345e6
Tristen Neu <39311873+tristenneu@users.noreply.github.com>
2018-12-03 19:18:36 -0600

    user profile pic dropdown (#107)

--- reactapp/src/components/_Nav/DropDown/DropDown.js
+++ reactapp/src/components/_Nav/DropDown/DropDown.js
@@ -1,59 +1,96 @@
 import React from 'react';
-import {
-	Dropdown,
-	DropdownToggle,
-	DropdownMenu,
-	DropdownItem
-} from 'reactstrap';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
 
 const logoutURL =
-	(process.env.REACT_APP_BACKEND || `http://localhost:5000`) + `/signout`;
+  (process.env.REACT_APP_BACKEND || `http://localhost:5000`) + `/signout`;
+  
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  profilepic: {
+    width: '40px',
+    height: '40px',
+    borderRadius: '50%'
+  }
+});
 
 class DropDown extends React.Component {
-	constructor(props) {
-		super(props);
+  state = {
+    open: false,
+  };
 
-		this.toggle = this.toggle.bind(this);
-		this.state = {
-			dropdownOpen: false
-		};
-	}
+  handleToggle = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
 
-	toggle() {
-		this.setState(prevState => ({
-			dropdownOpen: !prevState.dropdownOpen
-		}));
-	}
+  handleClose = event => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
 
-	render() {
-		return (
-			<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-				<DropdownToggle caret>Menu</DropdownToggle>
-				<DropdownMenu>
-					<DropdownItem>
-						<Link to={`/ProjectList`}>My Profile</Link>
-					</DropdownItem>
-					<DropdownItem>
-						<Link to={`/settings`}>Profile Settings</Link>
-					</DropdownItem>
-					<DropdownItem>
-						<Link to={`/newproject`}>New Project</Link>
-					</DropdownItem>
-					{window.screen.width <= 500 ? (
-						<DropdownItem>
-							<Link to={logoutURL}>Signout</Link>
-						</DropdownItem>
-					) : null}
-				</DropdownMenu>
-			</Dropdown>
-		);
-	}
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+          <Button
+            buttonRef={node => {
+              this.anchorEl = node;
+            }}
+            aria-owns={open ? 'menu-list-grow' : undefined}
+            aria-haspopup="true"
+            onClick={this.handleToggle}
+            style={{ outline: 'none' }}
+          >
+            <img src={this.props.userInfo.img_url} className={classes.profilepic} />
+          </Button>
+          <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                id="menu-list-grow"
+                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={this.handleClose}>
+                    <MenuList>
+                      <MenuItem onClick={this.handleClose}><Link to={`/ProjectList`}>My Profile</Link></MenuItem>
+                      <MenuItem onClick={this.handleClose}><Link to={`/settings`}>Profile Settings</Link></MenuItem>
+                      <MenuItem onClick={this.handleClose}><Link to={`/newproject`}>New Project</Link></MenuItem>
+                      <MenuItem onClick={this.handleClose}><a href={logoutURL}>Signout</a></MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+      </div>
+    );
+  }
 }
+
 
 const mapStateToProps = state => ({
 	userInfo: state.loggedInReducer.userInfo
 });
 
-export default connect(mapStateToProps)(DropDown);
+export default compose(
+	withStyles(styles),
+	connect(mapStateToProps)
+)(DropDown);

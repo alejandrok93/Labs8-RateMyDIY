9b23233c91ebd52f3199539d0e89a555eaf28ca5
alejandrok93 <alejandrok93@gmail.com>
2018-12-04 13:38:48 -0800

    added user img_url to the LandingPageModel getPopularProjects function

--- server/models/landingPageModel.js
+++ server/models/landingPageModel.js
@@ -1,32 +1,35 @@
 const db = require('../config/dbConfig');
 
 module.exports = {
-  getPopularProjects,
-  getPopularMakers,
-  getPopularReviewers
+	getPopularProjects,
+	getPopularMakers,
+	getPopularReviewers
 };
 
 function getPopularProjects() {
-  return db('projects')
-    .join("users", "projects.user_id", "users.user_id")
-    .select("projects.img_url",
-      "projects.user_id",
-      "projects.project_name",
-      "users.username",
-      "projects.project_id",
-      "projects.project_rating")
-    .orderBy('project_rating', 'desc')
-    .limit(4)
+	return db('projects')
+		.join('users', 'projects.user_id', 'users.user_id')
+		.select(
+			'projects.img_url',
+			'projects.user_id',
+			'projects.project_name',
+			'users.username',
+			'users.img_url',
+			'projects.project_id',
+			'projects.project_rating'
+		)
+		.orderBy('project_rating', 'desc')
+		.limit(4);
 }
 
 function getPopularMakers() {
-  return db('users')
-    .orderBy('user_rating', 'desc')
-    .limit(4)
+	return db('users')
+		.orderBy('user_rating', 'desc')
+		.limit(4);
 }
 
 function getPopularReviewers() {
-  return db('users')
-    .orderBy('helpfulness', 'desc')
-    .limit(4)
+	return db('users')
+		.orderBy('helpfulness', 'desc')
+		.limit(4);
 }

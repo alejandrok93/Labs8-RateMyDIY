e08694d041d78f87829869c0a804e84a5e70b3ab
alejandrok93 <alejandrok93@gmail.com>
2019-03-13 13:29:23 -0700

    Added landing page tests

---
+++ reactapp/src/components/_LandingPage/LandingPage.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import LandingPage from './LandingPage';
+
+it('matches snapshot', () => {
+	const tree = renderer.create(<LandingPage />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+it('Landing Page renders without crashing', () => {
+	shallow(<LandingPage />);
+});

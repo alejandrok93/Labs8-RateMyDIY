cc7253203e2271d65be1ec9f2a0ca78acf545694
alejandrok93 <alejandrok93@gmail.com>
2019-03-25 14:15:02 -0700

    Added dotenv for reactapp

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -14,6 +14,7 @@ import { App } from './components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Styles
 import './index.css';
+require('dotenv').config();
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
